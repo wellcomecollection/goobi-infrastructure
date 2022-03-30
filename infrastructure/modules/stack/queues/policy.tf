8924c5e462a9e989c07d1fcc1d53d38b083b45d9
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2022-03-30 18:00:08 +0200

    add bagit queue to general queue access policy

--- infrastructure/modules/stack/queues/policy.tf
+++ infrastructure/modules/stack/queues/policy.tf
@@ -12,7 +12,8 @@ data "aws_iam_policy_document" "read_write_queue" {
     resources = [
       aws_sqs_queue.goobi_job.arn,
       aws_sqs_queue.goobi_command.arn,
-      aws_sqs_queue.goobi_job_dlq.arn
+      aws_sqs_queue.goobi_job_dlq.arn,
+      aws_sqs_queue.goobi_bagit_job.arn
     ]
   }
 
