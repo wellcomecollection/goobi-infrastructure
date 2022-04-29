690c89ebc87280df11f0cf44b66a3a553064880d
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2022-04-29 18:41:24 +0200

    enable managed termination protection
    this should prevent EC2 instances to be terminated while they are still running ECS tasks

--- infrastructure/modules/ec2_capacity_provider/main.tf
+++ infrastructure/modules/ec2_capacity_provider/main.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_capacity_provider" "capacity_provider" {
 
   auto_scaling_group_provider {
     auto_scaling_group_arn         = aws_autoscaling_group.asg.arn
-    managed_termination_protection = "DISABLED"
+    managed_termination_protection = "ENABLED"
 
     managed_scaling {
       status                    = "ENABLED"
