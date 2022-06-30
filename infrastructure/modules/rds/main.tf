2120fdcf596c909472b06160d96633d1af36ae5e
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2022-06-30 10:19:21 +0200

    set final snapshot

--- infrastructure/modules/rds/main.tf
+++ infrastructure/modules/rds/main.tf
@@ -15,15 +15,17 @@ resource "aws_db_subnet_group" "default" {
 }
 
 resource "aws_rds_cluster" "default" {
-  db_subnet_group_name    = aws_db_subnet_group.default.name
-  cluster_identifier      = var.cluster_identifier
-  database_name           = var.database_name
-  master_username         = var.username
-  master_password         = var.password
-  vpc_security_group_ids  = [aws_security_group.database_sg.id]
-  backup_retention_period = var.backup_retention_period
-  deletion_protection     = var.deletion_protection
-  engine_version          = var.engine_version
-  engine                  = var.engine
+  db_subnet_group_name      = aws_db_subnet_group.default.name
+  cluster_identifier        = var.cluster_identifier
+  database_name             = var.database_name
+  master_username           = var.username
+  master_password           = var.password
+  vpc_security_group_ids    = [aws_security_group.database_sg.id]
+  backup_retention_period   = var.backup_retention_period
+  deletion_protection       = var.deletion_protection
+  engine_version            = var.engine_version
+  engine                    = var.engine
+  final_snapshot_identifier = "${var.cluster_identifier}-final"
+  skip_final_snapshot       = false
 }
 
