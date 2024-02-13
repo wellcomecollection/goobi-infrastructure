90c533debcaf6cd918b4cc87d1638deb69ca4c6a
Robert Kenny <r.kenny@wellcome.org>
2024-02-13 09:31:07 +0000

    update base amis

--- infrastructure/staging/locals.tf
+++ infrastructure/staging/locals.tf
@@ -5,4 +5,26 @@ locals {
   ia_username_key     = "workflow/ia_username"
   ia_password_key     = "workflow/ia_password"
   account_id_digirati = "653428163053"
+
+  # The following are the AMI IDs for the latest Amazon Linux 2 ECS-optimised AMI
+  container_host_ami_id = data.aws_ami.container_host_ami.image_id
+  bastion_host_ami_id   = data.aws_ami.bastion_host_ami.image_id
+}
+
+data "aws_ami" "container_host_ami" {
+  most_recent = true
+  owners      = ["self"]
+  filter {
+    name   = "name"
+    values = ["weco-amzn2-ecs-optimised-hvm-x86_64*"]
+  }
+}
+
+data "aws_ami" "bastion_host_ami" {
+  most_recent = true
+  owners      = ["self"]
+  filter {
+    name   = "name"
+    values = ["weco-amzn2-hvm-x86_64*"]
+  }
 }
