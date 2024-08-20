34e8974800656988534de3818291eeb75d6724bb
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2024-08-20 15:25:23 +0200

    move ep lambda to wellcome module for lambdas

--- infrastructure/staging/lambdas.tf
+++ infrastructure/staging/lambdas.tf
@@ -1,25 +1,24 @@
-data "aws_s3_object" "lambda_s3_trigger_goobi_package" {
-  bucket = data.aws_s3_bucket.workflow-infra.bucket
-  key    = "lambdas/s3_trigger_goobi.zip"
-}
-
-resource "aws_lambda_function" "lambda_s3_trigger_goobi_stage_ep" {
+module "lambda_s3_trigger_goobi_ep" {
+  source = "git::https://github.com/wellcomecollection/terraform-aws-lambda.git//?ref=v1.2.0"
+  name = "${local.environment_name}-lambda_s3_trigger_goobi_ep"
+  runtime = "python3.9"
+  handler = "s3_trigger_goobi.lambda_handler"
   description   = "lambda to call Goobi API for import after successful S3 upload"
-  function_name = "s3_trigger_goobi_stage_ep"
-
   s3_bucket         = data.aws_s3_object.lambda_s3_trigger_goobi_package.bucket
   s3_key            = data.aws_s3_object.lambda_s3_trigger_goobi_package.key
   s3_object_version = data.aws_s3_object.lambda_s3_trigger_goobi_package.version_id
-
-  role    = aws_iam_role.lambda_stage_iam_role.arn
-  handler = "s3_trigger_goobi.lambda_handler"
-  runtime = "python3.9"
   timeout = "60"
+  vpc_config = {
+    security_group_ids = [
+      aws_security_group.interservice.id,
+      aws_security_group.service_egress.id,
+    ]
+
+    subnet_ids = module.network.private_subnets
+  }
   publish = true
-
   memory_size = "128"
-
-  environment {
+  environment = {
     variables = {
       API_ENDPOINT     = local.lambda_api_endpoint_ep
       TOKEN            = local.lambda_token_ep
@@ -28,30 +27,22 @@ resource "aws_lambda_function" "lambda_s3_trigger_goobi_stage_ep" {
       HOTFOLDER        = "hotfolder"
     }
   }
+}
 
-  vpc_config {
-    security_group_ids = [
-      aws_security_group.interservice.id,
-      aws_security_group.service_egress.id,
-    ]
 
-    subnet_ids = module.network.private_subnets
-  }
+data "aws_s3_object" "lambda_s3_trigger_goobi_package" {
+  bucket = data.aws_s3_bucket.workflow-infra.bucket
+  key    = "lambdas/s3_trigger_goobi.zip"
 }
 
 resource "aws_lambda_permission" "allow_event_s3_trigger_goobi_stage_ep" {
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_s3_trigger_goobi_stage_ep.arn
+  function_name = module.lambda_s3_trigger_goobi_ep.lambda.function_name
   principal     = "s3.amazonaws.com"
   source_arn    = aws_s3_bucket.workflow-stage-upload.arn
 }
 
-resource "aws_cloudwatch_log_group" "cloudwatch_log_group_s3_trigger_goobi_stage_ep" {
-  name = "/aws/lambda/s3_trigger_goobi_stage_ep"
-
-  retention_in_days = "14"
-}
 
 resource "aws_lambda_function" "lambda_s3_trigger_goobi_stage_digitised" {
   description   = "lambda to call Goobi API for import after successful S3 upload"
--- infrastructure/staging/s3.tf
+++ infrastructure/staging/s3.tf
@@ -183,14 +183,14 @@ resource "aws_s3_bucket_notification" "bucket_notification_workflow-stage-upload
   bucket = aws_s3_bucket.workflow-stage-upload.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.lambda_s3_trigger_goobi_stage_ep.arn
+    lambda_function_arn = module.lambda_s3_trigger_goobi_ep.lambda.arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "editorial/"
     filter_suffix       = ".zip"
   }
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.lambda_s3_trigger_goobi_stage_ep.arn
+    lambda_function_arn = module.lambda_s3_trigger_goobi_ep.lambda.arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "editorial/"
     filter_suffix       = ".ZIP"
